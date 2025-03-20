e3184020ee62c6aedf21a576b17690b0e5854ea1
Ryan Mossor <ryan.mossor@gmail.com>
2025-03-20 17:48:49 -0500

    update quote widget

--- src/typescript/quotes/notifyTodaysQuote.ts
+++ src/typescript/quotes/notifyTodaysQuote.ts
@@ -1,20 +1,10 @@
 import * as tasker from "../dev/tasker";
 import { Quote, QuotesJson } from "../dev/types";
-import { isEnvTasker, isNullOrEmpty, readJsonData } from "../modules/utils";
-
-export function notifyTodaysQuote(quotesJson: QuotesJson) {
-    const todaysQuote: Quote = quotesJson.daily.quotes.find((x) => x.id === quotesJson.daily.todaysQuoteId);
-
-    const formattedQuote = isNullOrEmpty(todaysQuote.author)
-        ? `"${todaysQuote.quote}"`
-        : `"${todaysQuote.quote}" - ${todaysQuote.author}`;
-
-    return formattedQuote;
-}
+import { isEnvTasker, readJsonData } from "../modules/utils";
 
 if (isEnvTasker()) {
-    const quotesJson = readJsonData<QuotesJson>({ filename: "quotes.json" });
-    const todaysQuote = notifyTodaysQuote(quotesJson.data);
-    tasker.setLocal("todays_quote", todaysQuote);
+    const quotesJson: QuotesJson = readJsonData<QuotesJson>({ filename: "quotes.json" }).data;
+    const todaysQuote: Quote = quotesJson.daily.quotes.find((x) => x.id === quotesJson.daily.todaysQuoteId);
+    tasker.setLocal("todays_quote", JSON.stringify(todaysQuote));
     tasker.exit();
 }
